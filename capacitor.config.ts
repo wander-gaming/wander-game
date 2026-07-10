5e3b24e2844df8535d5c7688c8f3d8a48a99476a
samuel-asleep <benneu40@gmail.com>
2026-07-10 22:40:08 +0100

    bump capacitor ver

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from "@capacitor/cli";
-
-const config: CapacitorConfig = {
-  appId: "com.wandergame.app",
-  appName: "Wander Game",
-  webDir: "dist",
-};
-
-export default config;
